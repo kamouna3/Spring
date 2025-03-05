282927555385e9634cac815714283de815f977f9
kamouna3 <hafsaeljaroudi@gmail.com>
2025-03-05 16:11:42 +0000

    initial push bransh hafsa

---
+++ src/test/java/com/example/spring_boot_intellij_ce/SpringBootIntellijCeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_intellij_ce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntellijCeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
